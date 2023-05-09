b122d20f9a6025d7a74f32ae45985047968d0394
LeeCunningham99 <leecunningham00@gmail.com>
2023-05-09 12:24:35 +0100

    Added files for states
    Add several state files

---
+++ RoboCatSFMLClient/StateID.hpp
@@ -0,0 +1,18 @@
+#pragma once
+enum class StateID
+{
+	kNone,
+	kTitle,
+	kMenu,
+	kGame,
+	kPause,
+	kSettings,
+	kLevelWin,
+	kLevelLose,
+	kNetworkPause,
+	kJoinSettings,
+	kNetworkGame,
+	kMultiplayerWin,
+	kMultiplayerLose,
+	kLobby
+};
