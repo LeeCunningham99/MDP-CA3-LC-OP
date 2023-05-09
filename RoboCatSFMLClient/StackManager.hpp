df1f0a1cdca576c2cc5ab3bf6bb4511c038bfe53
LeeCunningham99 <leecunningham00@gmail.com>
2023-05-09 12:40:30 +0100

    added files to PCH
    Added files to PCH

---
+++ RoboCatSFMLClient/StackManager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+class StackManager
+{
+public:
+	static bool StaticInit();
+	void PushState(StateID title);
+	void Update(float delta_time);
+	bool IsEmpty() const;
+	void HandleEvent(const sf::Event& p_event);
+	void Render() const;
+	void PopState();
+	void ClearStates();
+	static std::unique_ptr<StackManager> sInstance;
+	State* GetCurrentState() const;
+
+private:
+	StackManager();
+
+	StateStack m_stack;
+};
