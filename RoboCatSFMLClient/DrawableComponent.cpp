ab9012767396a41a945cfc87b0509ace7e6aa049
LeeCunningham99 <leecunningham00@gmail.com>
2023-05-10 12:33:15 +0100

    added cpp files
    Added relevant cpp files

---
+++ RoboCatSFMLClient/DrawableComponent.cpp
@@ -0,0 +1,43 @@
+#include "RoboCatClientPCH.hpp"
+
+DrawableComponent::DrawableComponent(GameObject* game_object)
+	: m_game_object(game_object),
+	m_draw_predicate([] { return true; }),
+	m_offset(0, 0)
+{
+	//and add yourself to the rendermanager...
+	RenderManager::sInstance->AddComponent(this);
+}
+
+DrawableComponent::~DrawableComponent()
+{
+	//don't render me, I'm dead!
+	RenderManager::sInstance->RemoveComponent(this);
+}
+
+void DrawableComponent::UpdatePosition()
+{
+	const auto pos = m_game_object->GetLocation();
+	const auto rot = m_game_object->GetRotation();
+	setPosition(pos.mX + m_offset.x, pos.mY + m_offset.y);
+	setRotation(rot);
+}
+
+void DrawableComponent::SetDrawPredicate(const std::function<bool()>& predicate)
+{
+	m_draw_predicate = predicate;
+}
+
+void DrawableComponent::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	if (!m_draw_predicate())
+		return;
+
+	states.transform *= getTransform();
+	DrawCurrent(target, states);
+}
+
+void DrawableComponent::SetOffset(const sf::Vector2f& offset)
+{
+	m_offset = offset;
+}
--- RoboCatSFMLClient/SpriteComponent.cpp
+++ RoboCatSFMLClient/SpriteComponent.cpp
@@ -1,35 +1,41 @@
 #include "RoboCatClientPCH.hpp"
 
 
-SpriteComponent::SpriteComponent(GameObject* inGameObject) :
-	mGameObject(inGameObject)
+SpriteComponent::SpriteComponent(GameObject* inGameObject)
+	: DrawableComponent(inGameObject)
 {
-	//and add yourself to the rendermanager...
-	RenderManager::sInstance->AddComponent(this);
 }
 
-SpriteComponent::~SpriteComponent()
+void SpriteComponent::SetTexture(const TexturePtr& inTexture, const sf::IntRect subRect, bool is_player)
 {
-	//don't render me, I'm dead!
-	RenderManager::sInstance->RemoveComponent(this);
-}
+	if (subRect != sf::IntRect(0, 0, 0, 0))
+		m_sprite.setTextureRect(subRect);
 
-void SpriteComponent::SetTexture(TexturePtr inTexture)
-{
-	auto tSize = inTexture->getSize();
 	m_sprite.setTexture(*inTexture);
-	m_sprite.setOrigin(tSize.x / 2, tSize.y / 2);
-	m_sprite.setScale(sf::Vector2f(1.f * mGameObject->GetScale(), 1.f * mGameObject->GetScale()));
+	Utility::CentreOrigin(*this, is_player);
+	m_sprite.setScale(sf::Vector2f(m_game_object->GetScale(), m_game_object->GetScale()));
 }
 
 sf::Sprite& SpriteComponent::GetSprite()
 {
-	// Update the sprite based on the game object stuff.
-	auto pos = mGameObject->GetLocation();
-	auto rot = mGameObject->GetRotation();
-	m_sprite.setPosition(pos.mX, pos.mY);
-	m_sprite.setRotation(rot);
-
 	return m_sprite;
 }
 
+void SpriteComponent::DrawCurrent(sf::RenderTarget& target, const sf::RenderStates states) const
+{
+	target.draw(m_sprite, states);
+	// const sf::FloatRect bounding_rect = m_game_object->GetBoundingRect();
+	// DrawBoundingRect(target, states, bounding_rect);
+}
+
+void SpriteComponent::DrawBoundingRect(sf::RenderTarget& target, sf::RenderStates states,
+	const sf::FloatRect& bounding_rect)
+{
+	sf::RectangleShape shape;
+	shape.setPosition(sf::Vector2f(bounding_rect.left, bounding_rect.top));
+	shape.setSize(sf::Vector2f(bounding_rect.width, bounding_rect.height));
+	shape.setFillColor(sf::Color::Transparent);
+	shape.setOutlineColor(sf::Color::Green);
+	shape.setOutlineThickness(1.f);
+	target.draw(shape);
+}
