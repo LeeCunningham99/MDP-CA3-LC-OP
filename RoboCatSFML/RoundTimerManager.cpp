ee66ae3f202e6c04d16639aa78e9c06623356c6f
omcpeake <72383307+omcpeake@users.noreply.github.com>
2023-05-19 16:30:08 +0100

    Implemented methods to rtManager
    implemented some methods to the round timer manager

--- RoboCatSFML/RoundTimerManager.cpp
+++ RoboCatSFML/RoundTimerManager.cpp
@@ -1,2 +1,24 @@
 #include "RoboCatPCH.hpp"
 #include "RoundTimerManager.hpp"
+
+void RoundTimerManager::SetTimer(int inTimer)
+{
+	mTimer = inTimer;
+	/*char	buffer[256];
+	snprintf(buffer, 256, "%i", mTimer);
+	mFormattedTimer = string(buffer);*/
+}
+
+bool RoundTimerManager::Write(OutputMemoryBitStream& inOutputStream) const
+{
+	inOutputStream.Write(mTimer);
+	return true;
+}
+
+bool RoundTimerManager::Read(InputMemoryBitStream& inInputStream)
+{
+	inInputStream.Read(mTimer);
+	return false;
+}
+
+
--- RoboCatSFML/RoundTimerManager.hpp
+++ RoboCatSFML/RoundTimerManager.hpp
@@ -1,14 +1,28 @@
 #pragma once
 class RoundTimerManager
 {
+	//static void StaticInit();
+	//static std::unique_ptr< RoundTimerManager >	sInstance;
+
+
 	float GetTimer()		const { return mTimer; }
 
 
 	void SetTimer(int inTimer);
 
+	//static float	GetSerializedSize();
+
+	const string& GetFormattedTimer()	const { return mFormattedTimer; }
+
+
+	bool			Write(OutputMemoryBitStream& inOutputStream) const;
+	bool			Read(InputMemoryBitStream& inInputStream);
+
+
 
 
 private:
 	float mTimer;
+	string mFormattedTimer;
 };
 
